c02845c1f8f814712684f13c24e8c75e2587a926
Patrik "EUcreepemineCZ" Neumann <patrik.neu@centrum.cz>
2024-06-20 18:08:11 +0200

    Update textStrings.js

--- js/textStrings.js
+++ js/textStrings.js
@@ -725,100 +725,100 @@ var textStrings_CZ = {
 	// Act 1
 	"niceHat": "OOH Krásná čepka",
 	"outtaHere": "Jo, jdi pryč",
-	"getARoom": "GROSS, GO GET A ROOM",
-	"notCoolAnymore": "nvm hats aren't cool anymore",
-	"tvOnTv": "A TV... ON TV!",
-	"cricky": "LIL' CRICKY <3",
-	"tooManyCrickets": "okay that's too many crickets",
-	"normalPeep": "just a normal peep",
-	"normalPeeps": "just some normal peeps",
-	"wowNothing": "WOWWEE, IT'S NOTHING",
+	"getARoom": "HRUBÝ, BĚŽTE DO POKOJE",
+	"notCoolAnymore": "nevadí, čepice už nejsou cool",
+	"tvOnTv": "TV... V TV!",
+	"cricky": "MALÝ CVRČEK <3",
+	"tooManyCrickets": "ok, příliš hodně cvrčků",
+	"normalPeep": "prostě obyčejný peep",
+	"normalPeeps": "Jen nějakí obyčejní peepové",
+	"wowNothing": "WOWWEE, NENÍ TO NIC",
 
 	// Act 2
-	"crazySquareAttacks": "CRAZED SQUARE ATTACKS",
-	"justMissed": "oooooh just missed it",
-	"somethingInteresting": "(ya gotta catch 'em doing *something* interesting...)",
-	"whoIsScreaming": "(ya gotta catch who's screaming at 'em)",
-	"circleFearsSquares": "CIRCLE FEARS SQUARES",
-	"whoScaresThem": "(ya gotta also catch *who* they're scared by)",
+	"crazySquareAttacks": "POBLÁZNĚNÉ ČTVERCE ÚTOČÍ",
+	"justMissed": "oooooh propásnuto",
+	"somethingInteresting": "(musíš je chytit při *něčem* zajímavém...)",
+	"whoIsScreaming": "(musíš chytit toho, kdo na ně křičí)",
+	"circleFearsSquares": "KRUHY SE BOJÍ ČTVERCŮ",
+	"whoScaresThem": "(musíš taky chytit *kým* jsou strašeni)",
 	"areTheyScared": "(ya gotta catch 'em *being* scared by a square)", // not used
-	"squaresSnubCircles": "SQUARES SNUB CIRCLES",
+	"squaresSnubCircles": "ČTVERCE URÁŽÍ KRUHY",
 	"areTheySnubbed": "(ya gotta catch 'em *while* snubbing a circle)", // not used
-	"everyoneHates": "EVERYONE HATES EVERYONE!!1!",
-	"almostEveryoneHates": "ALMOST EVERYONE HATES EVERYONE...",
-	"squaresHateCircles": "SQUARES HATE CIRCLES",
-	"circlesHateSquares": "CIRCLES HATE SQUARES",
+	"everyoneHates": "VŠICHNI NENÁVIDÍ VŠECHNY!!1!",
+	"almostEveryoneHates": "SKORO VŠICHNI NENÁVIDÍ VŠECHNY...",
+	"squaresHateCircles": "ČTVERCE NENÁVIDÍ KRUHY",
+	"circlesHateSquares": "KRUHY NENÁVIDÍ ČTVERCE",
 	"areTheyYelling": "(ya gotta catch 'em *yelling* at others)", // not used
-	"nerdsNow": "what are these nerds doing now",
-	"schockedPeep": "why's this peep shocked?",
-	"whatever": "whatever",
+	"nerdsNow": "co tito nerdi právě dělají",
+	"schockedPeep": "proč je tento peep v šoku?",
+	"whatever": "To je jedno",
 
 	// Act 3
 	"ellipsis": ". . .",
-	"coolNoMore": "hats ain't cool no more, haven't you heard?",
-	"beScared": "BE SCARED. BE ANGRY.",
+	"coolNoMore": "čepky už nejsou cool, neslyšels?",
+	"beScared": "BOJ SE. BUĎ NAŠTVANÝ.",
 
 	// MANIFESTO
 	"manifesto": [
 		//"as if you viewers want GOOD news"       ,
-		"who tunes in to watch *people get along?*",
-		"peace is boring. violence goes viral.",
+		"kdo se naladí, aby sledoval, *jak lidé spolu vycházejí?*",
+		"mír je nuda. násilí je virální.",
 		//"peace is boring. conflict gets clicks." ,
-		"and every story needs a conflict, so...",
+		"a každý příběh potřebuje konflikt, tak...",
 		//"...GIVE THE AUDIENCE WHAT THEY WANT."   ,
-		"GIVE THE AUDIENCE WHAT THEY WANT."
+		"DEJ DIVÁKŮM, CO CHTĚJÍ."
 	],
 
 	// -- Strings currently in graphics resources --
 
 	// (bg_preload.png) Preload screen
-	"playingTime": "playing time: 5 minutes",
-	"warning": "warning: the following program\ncontains scenes of snobbery,\nrudeness & mass murder.\nviewer discretion is advised.",
+	"playingTime": "doba hraní: 5 minut",
+	"warning": "varování: následující program\nobsahuje scény snobství,\nhrubosti & masové vraždy.\ndoporučujeme diskrétnost.",
 
 	// (cam-instructions.png) Camera Instructions
-	"pointAndClick": "POINT & CLICK",
+	"pointAndClick": "NAMIŘ & KLIKNI",
 
 	// (chyron3.png)
-	"chyronNothing": "WOWWEE, IT'S *NOTHING*",
+	"chyronNothing": "WOWWEE, NENÍ TO *NIC*",
 
 	// (credits0001.png)
-	"createdBy": "created by",
+	"createdBy": "vytvořil",
 	"NickyCase": "Nicky Case",
 
 	// (credits0002.png)
-	"manyThanks": "many thanks to my playtesters:",
+	"manyThanks": "děkuji všem testerům:",
 
 	// (credits000[3,4,5,6].png)
-	"patreonSupporters": "and to my patreon supporters:",
+	"patreonSupporters": "a patreon podporovatelům:",
 
 	// (credits0007.png)
-	"lastButNotLeast": "and last but not least,",
+	"lastButNotLeast": "a nakonec,",
 
 	// (credits0008.png)
-	"thankYouForPlaying": "thank YOU for playing!",
+	"thankYouForPlaying": "DĚKUJI za hraní!",
 
 	// (preload_play.png)
-	"playButton": "PLAY",
+	"playButton": "HRÁT",
 
 	// (end_button.png) Post Credits
-	"otherWorkButton": "see my other work",
-	"buyCoffeeButton": "buy me a coffee",
-	"replayButton": "replay this mess",
+	"otherWorkButton": "ostatní tvorba",
+	"buyCoffeeButton": "kup mi kávu",
+	"replayButton": "znovu hrát tento nepořádek",
 
 	// (logo.png) Post Credits
-	"logoWBWWB": "SHARE THE PAIN:",
+	"logoWBWWB": "SDÍLET BOLEST:",
 
 	// (end_prototype.png)
-	"endOfPrototype": "END OF PROTOTYPE",
-	"toBeContinued": "(to be continued!)",
+	"endOfPrototype": "KONEC PROTOTYPU",
+	"toBeContinued": "(bude pokračování!)",
 
 	// (quote0002.png)
-	"WBWWB": "We become what we behold.",
-	"WSOTATOTSU": "We shape our tools and then our tools shape us.",
+	"WBWWB": "Stáváme se tím, co vidíme.",
+	"WSOTATOTSU": "Tvarujeme naše nástroje a naše nástroje pak tvarují nás",
 
 	// (quote0003.png)
 	"MarshallMcLuhan": "Marshall McLuhan",
 
 	// (quote0004.png)
-	"misatrributed": "(misattributed)",
+	"misatrributed": "(špatně přisouzeno)",
 };
